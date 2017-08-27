44e239b077f1930f3ee261142c977b9c7abb8ec9
marenbo <my-chocolate@163.com>
2017-08-27 11:52:47 +0800

    Update the naming of variables

--- app/src/main/java/com/ryan/customview/view/RemoteControlView.java
+++ app/src/main/java/com/ryan/customview/view/RemoteControlView.java
@@ -27,15 +27,15 @@ public class RemoteControlView extends BaseView {
 
     private Context mContext;
 
-    private Path mPath1 = new Path();
-    private Path mPath2 = new Path();
-    private Path mPath3 = new Path();
-    private Path mPath4 = new Path();
+    private Path mPath_top = new Path();
+    private Path mPath_right = new Path();
+    private Path mPath_bottom = new Path();
+    private Path mPath_left = new Path();
     private Path mCirclePath = new Path();
-    private Region mRegion1 = new Region();
-    private Region mRegion2 = new Region();
-    private Region mRegion3 = new Region();
-    private Region mRegion4 = new Region();
+    private Region mRegion_top = new Region();
+    private Region mRegion_right = new Region();
+    private Region mRegion_bottom = new Region();
+    private Region mRegion_left = new Region();
     private Region mCircleRegion = new Region();
 
     private float mGap = 20;//扇形之间的间隙
@@ -60,37 +60,37 @@ public class RemoteControlView extends BaseView {
 
         RectF rectF = new RectF(centerX - mBigRadius, centerY - mBigRadius - mGap,
                 centerX + mBigRadius, centerY + mBigRadius - mGap);
-        mPath1.addArc(rectF, 225, 90);
-        mPath1.lineTo(centerX, centerY - mGap);
-        mPath1.close();
-        mPath1.op(mCirclePath, Path.Op.DIFFERENCE);
-        mRegion1.setPath(mPath1, globalRegion);
+        mPath_top.addArc(rectF, 225, 90);
+        mPath_top.lineTo(centerX, centerY - mGap);
+        mPath_top.close();
+        mPath_top.op(mCirclePath, Path.Op.DIFFERENCE);
+        mRegion_top.setPath(mPath_top, globalRegion);
 
 
         rectF = new RectF(centerX - mBigRadius + mGap, centerY - mBigRadius,
                 centerX + mBigRadius + mGap, centerY + mBigRadius);
-        mPath2.arcTo(rectF, -45, 90);
-        mPath2.lineTo(centerX + mGap, centerY);
-        mPath2.close();
-        mPath2.op(mCirclePath, Path.Op.DIFFERENCE);
-        mRegion2.setPath(mPath2, globalRegion);
+        mPath_right.arcTo(rectF, -45, 90);
+        mPath_right.lineTo(centerX + mGap, centerY);
+        mPath_right.close();
+        mPath_right.op(mCirclePath, Path.Op.DIFFERENCE);
+        mRegion_right.setPath(mPath_right, globalRegion);
 
         rectF = new RectF(centerX - mBigRadius, centerY - mBigRadius + mGap,
                 centerX + mBigRadius, centerY + mBigRadius + mGap);
-        mPath3.arcTo(rectF, 45, 90);
-        mPath3.lineTo(centerX, centerY + mGap);
-        mPath3.close();
-        mPath3.op(mCirclePath, Path.Op.DIFFERENCE);
-        mRegion3.setPath(mPath3, globalRegion);
+        mPath_bottom.arcTo(rectF, 45, 90);
+        mPath_bottom.lineTo(centerX, centerY + mGap);
+        mPath_bottom.close();
+        mPath_bottom.op(mCirclePath, Path.Op.DIFFERENCE);
+        mRegion_bottom.setPath(mPath_bottom, globalRegion);
 
 
         rectF = new RectF(centerX - mBigRadius - mGap, centerY - mBigRadius,
                 centerX + mBigRadius - mGap, centerY + mBigRadius);
-        mPath4.arcTo(rectF, 135, 90);
-        mPath4.lineTo(centerX - mGap, centerY);
-        mPath4.close();
-        mPath4.op(mCirclePath, Path.Op.DIFFERENCE);
-        mRegion4.setPath(mPath4, globalRegion);
+        mPath_left.arcTo(rectF, 135, 90);
+        mPath_left.lineTo(centerX - mGap, centerY);
+        mPath_left.close();
+        mPath_left.op(mCirclePath, Path.Op.DIFFERENCE);
+        mRegion_left.setPath(mPath_left, globalRegion);
 
         mCirclePath.reset();
         mCirclePath.addCircle(centerX, centerY, mSmallRaidus - mGap * (float) Math.sqrt(2), Path.Direction.CW);
@@ -107,14 +107,14 @@ public class RemoteControlView extends BaseView {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (mRegion1.contains(x, y)) {
+                if (mRegion_top.contains(x, y)) {
                     mCurrentSelected = 1;
 
-                } else if (mRegion2.contains(x, y)) {
+                } else if (mRegion_right.contains(x, y)) {
                     mCurrentSelected = 2;
-                } else if (mRegion3.contains(x, y)) {
+                } else if (mRegion_bottom.contains(x, y)) {
                     mCurrentSelected = 3;
-                } else if (mRegion4.contains(x, y)) {
+                } else if (mRegion_left.contains(x, y)) {
                     mCurrentSelected = 4;
                 } else if (mCircleRegion.contains(x, y)) {
                     mCurrentSelected = 5;
@@ -136,10 +136,10 @@ public class RemoteControlView extends BaseView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawArea(canvas, mPath1, 1);
-        drawArea(canvas, mPath2, 2);
-        drawArea(canvas, mPath3, 3);
-        drawArea(canvas, mPath4, 4);
+        drawArea(canvas, mPath_top, 1);
+        drawArea(canvas, mPath_right, 2);
+        drawArea(canvas, mPath_bottom, 3);
+        drawArea(canvas, mPath_left, 4);
         drawArea(canvas, mCirclePath, 5);
     }
 
