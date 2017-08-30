db2647e5e9da07ae7a8ee94807261d62fd6e49e4
renbo <renbo.ma@hypers.com>
2017-08-30 18:33:41 +0800

    update content

--- app/src/main/java/com/ryan/customview/view/TestView.java
+++ app/src/main/java/com/ryan/customview/view/TestView.java
@@ -2,14 +2,9 @@ package com.ryan.customview.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Path;
-import android.graphics.RectF;
+import android.graphics.Color;
 import android.graphics.Region;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 
 /**
  * Created by renbo on 2017/8/22.
@@ -40,115 +35,27 @@ public class TestView extends BaseView {
         }
     }
 
-    private Path mPath = new Path();
+
     private Region mRegion = new Region();
-    private Region gloabalRegion = new Region();
-    private GestureDetector mGestureDetector;
-
     private void init(Context context) {
-        mGestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent e) {
-                Log.d("TestView", "onDown");
-                return true;
-            }
-
-            @Override
-            public void onShowPress(MotionEvent e) {
-                Log.d("TestView", "showPress");
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                Log.d("TestView", "onSingleTapUp");
-                return true;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                Log.d("TestView", "onScroll");
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-                Log.d("TestView", "onLongPress");
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                Log.d("TestView", "onFling");
-                return true;
-            }
-        });
-        mGestureDetector.setIsLongpressEnabled(true);
-        mGestureDetector.setOnDoubleTapListener(new GestureDetector.OnDoubleTapListener() {
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public boolean onDoubleTap(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public boolean onDoubleTapEvent(MotionEvent e) {
-                return false;
-            }
-        });
-        setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return mGestureDetector.onTouchEvent(event);
-            }
-        });
-
-//        setLayerType(LAYER_TYPE_SOFTWARE, null);
-
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+//        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setShadowLayer(50, 50, 50, Color.RED);
     }
-
 
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mPath.addCircle(w / 2, h / 2, 500, Path.Direction.CW);
-        Log.d("TestView", "w:" + w);
-        Log.d("TestView", "h:" + h);
-        RectF rectF = new RectF();
-        mPath.computeBounds(rectF, true);
-        gloabalRegion = new Region(0, 0,
-                w, h);
-        mRegion.setPath(mPath, gloabalRegion);
     }
-
-
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        switch (event.getActionMasked()) {
-//            case MotionEvent.ACTION_DOWN:
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                break;
-//            case MotionEvent.ACTION_UP:
-//                break;
-//            case MotionEvent.ACTION_POINTER_DOWN:
-//                break;
-//            case MotionEvent.ACTION_POINTER_UP:
-//                break;
-//        }
-//        return super.onTouchEvent(event);
-//    }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawAuxiliary(canvas);
-        canvas.drawCircle(0, 0, 100, mPaint);
+        canvas.drawCircle(mWidth / 2, mHeight / 2, 100, mPaint);
+
     }
 
 
