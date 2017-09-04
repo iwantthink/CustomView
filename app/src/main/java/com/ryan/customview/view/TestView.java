92b0911f1c7d63bca9cc670ca434845e4bea6043
renbo <renbo.ma@hypers.com>
2017-09-04 18:31:05 +0800

    update TestView

--- app/src/main/java/com/ryan/customview/view/TestView.java
+++ app/src/main/java/com/ryan/customview/view/TestView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
@@ -40,46 +41,64 @@ public class TestView extends BaseView {
     }
 
     private Bitmap dst, src;
+    private Paint mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     private void init(Context context) {
         setLayerType(LAYER_TYPE_SOFTWARE, null);
-        mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setStrokeWidth(30);
-        mPaint.setStrokeJoin(Paint.Join.MITER);
-        mPath.lineTo(500, 500);
-        mPath.lineTo(700, 200);
-        Canvas canvas = new Canvas();
-        dst = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
-        canvas.setBitmap(dst);
-        mPaint.setColor(Color.BLUE);
-        canvas.drawCircle(100, 100, 100, mPaint);
-        src = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
-        canvas.setBitmap(src);
+//        mPaint.setStyle(Paint.Style.FILL);
+//        mPaint.setStrokeWidth(30);
+//        mPaint.setStrokeJoin(Paint.Join.MITER);
+////        mPath.lineTo(500, 500);
+////        mPath.lineTo(700, 200);
+//        Canvas canvas = new Canvas();
+//        dst = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+//        canvas.setBitmap(dst);
+//        mPaint.setColor(Color.BLUE);
+//        canvas.drawCircle(100, 100, 100, mPaint);
+//        src = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+//        canvas.setBitmap(src);
+        mCirclePaint.setStyle(Paint.Style.FILL);
+        mCirclePaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setColor(Color.RED);
-        canvas.drawRect(0, 0, 200, 200, mPaint);
+        mPaint.setStrokeWidth(20);
+        mMatrix = new Matrix();
 
     }
 
     private Path mPath = new Path();
+    private float[] mPoint = new float[2];
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        mPoint[0] = event.getX();
+//        mPoint[1] = event.getY();
+//        mMatrix.mapPoints(mPoint);
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                mPath.moveTo(mPoint[0], mPoint[1]);
+//                break;
+//            case MotionEvent.ACTION_MOVE:
+//                mPath.lineTo(mPoint[0], mPoint[1]);
+//                break;
+//        }
+//        postInvalidate();
+//        return true;
+//    }
+
+    private Matrix mMatrix;
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawAuxiliary(canvas);
-
-
         int layerID = canvas.saveLayer(0, 0, mWidth, mHeight, mPaint, Canvas.ALL_SAVE_FLAG);
         canvas.translate(mWidth / 2, mHeight / 2);
-
-        canvas.drawBitmap(dst, -100, -100, mPaint);
-//        mPaint.setColor(Color.BLUE);
-//        canvas.drawCircle(0, 0, 200, mPaint);
-        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
-        canvas.drawBitmap(src, 0, -100, mPaint);
-
-//        mPaint.setColor(Color.YELLOW);
-//        canvas.drawRect(0, 100, 200, -100, mPaint);
-        mPaint.setXfermode(null);
+        canvas.drawCircle(100, 100, 100, mCirclePaint);//dst
+        mCirclePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+        mCirclePaint.setColor(Color.RED);
+        canvas.drawRect(0, 0, 200, 200, mCirclePaint);//src
+        mCirclePaint.setXfermode(null);
         canvas.restoreToCount(layerID);
 
 
